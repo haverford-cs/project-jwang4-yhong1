f5b53daf981292b68c074726f99c390cf5a0126f
Andy <yuxuanh55@gmail.com>
2019-12-10 12:20:44 -0500

    correct typos fr util & update svm without skfolds

--- svm.py
+++ svm.py
@@ -4,41 +4,30 @@ Initialize SVM
 '''
 import util
 from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
 
 
 def main():
     opts = util.parse_args()
     X, y = util.data_load(opts.dataset)
-    skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
     model=SVC()
-    #old grid
+
     param_grid = {"C": [1, 10, 100, 1000], "gamma": [1e-4,1e-3,1e-2,1e-1,1]}
-    #new grid
-    #param_grid = {"C": [1, 10, 100, 1000], "gamma": [1e-4,1e-3,1e-2,1e-1]}
-    for train_index, test_index in skf.split(X, y):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        X_train, X_test = util.normalize(X_train, X_test)
 
-        grid=GridSearchCV(model, param_grid,cv=3, verbose=4,iid=False)
-        grid.fit(X_train,y_train)
-        grid_predictions=grid.predict(X_test)
-        cm=confusion_matrix(y_test,grid_predictions)
-        report=classification_report(y_test,grid_predictions)
-        
-        ''' 
-        without doing the gridsearch just using the default params - bad results
-        model.fit(X_train,y_train)
-        pred=model.predict(X_test)
-        cm=confusion_matrix(y_test,pred)
-        report=classification_report(y_test,pred)
-        '''
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42)
+    X_train, X_test = util.normalize(X_train, X_test)
 
-        print(cm)
-        print(report)
-        print("Best_Param_Estimator:", grid.best_estimator_)
+    grid=GridSearchCV(model, param_grid,cv=3, verbose=4,iid=False)
+    grid.fit(X_train,y_train)
+    grid_predictions=grid.predict(X_test)
+    cm=confusion_matrix(y_test,grid_predictions)
+    report=classification_report(y_test,grid_predictions)
+
+    print(cm)
+    print(report)
+    print("Best_Param_Estimator:", grid.best_estimator_)
 
 
 if __name__ == '__main__':
